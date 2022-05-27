5a8853dfb1adde69882ee8bb491980a1cf3adbe9
HamidReza Mohseniyan <82676693+jxroot@users.noreply.github.com>
2022-05-27 16:06:08 +0430

    Create getops.js

---
+++ asset/js/project/getops.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var url = window.location.hash
+    var repl = url.replace("#", "")
+    $("img").attr("src", "../" + repl)
+    $('img').click(function (e) {
+        var offset = $(this).offset();
+        var X = Math.round(e.pageX - offset.left);
+        var Y = Math.round(e.pageY - offset.top);
+        alert('X: ' + X + ', Y: ' +Y);
+    });
+    $('img').bind("contextmenu", function (e) {
+        return false;
+    });
+
+    $('img').mousedown(function (e) {
+        if (e.which == 3) {
+            var offset = $(this).offset();
+            var X = Math.round(e.pageX - offset.left);
+            var Y = Math.round(e.pageY - offset.top);
+        
+            $.post("../process/request.php", { click: true,x:X,y:Y }, function (data) {
+  
+              });
+        }
+    });
+
+});
